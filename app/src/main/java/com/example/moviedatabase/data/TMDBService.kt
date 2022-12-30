ac49e137732c9ea0fbf3f56528faa18b5afe23a4
michalgwo <114614618+michalgwo@users.noreply.github.com>
2022-12-30 18:16:32 +0100

    Created retrofit service interface

---
+++ app/src/main/java/com/example/moviedatabase/data/TMDBService.kt
@@ -0,0 +1,16 @@
+package com.example.moviedatabase.data
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+    @GET("/movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String): Response<MovieList>
+
+    @GET("/tv/popular")
+    suspend fun getPopularTvShows(@Query("api_key") apiKey: String): Response<TvShowList>
+
+    @GET("/person/popular")
+    suspend fun getPopularArtists(@Query("api_key") apiKey: String): Response<ArtistList>
+}
