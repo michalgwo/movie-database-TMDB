9e59e0409aa0963fb2c4b7817d8ce0c5ac83b5f9
michalgwo <114614618+michalgwo@users.noreply.github.com>
2022-12-30 17:41:07 +0100

    Created data classes for movies, artists and tv shows

---
+++ app/src/main/java/com/example/moviedatabase/data/ArtistList.kt
@@ -0,0 +1,15 @@
+package com.example.moviedatabase.data
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ArtistList(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val artists: List<Artist>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
---
+++ app/src/main/java/com/example/moviedatabase/data/MovieList.kt
@@ -0,0 +1,13 @@
+package com.example.moviedatabase.data
+import com.google.gson.annotations.SerializedName
+
+data class MovieList(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val movies: List<Movie>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
