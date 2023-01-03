7f8fff5ed9b85b7ccb1b2b427b17d14cf7e82a4e
michalgwo <114614618+michalgwo@users.noreply.github.com>
2023-01-03 22:32:32 +0100

    Implemented update movies functionality

--- app/src/main/java/com/example/moviedatabase/presentation/movies/MoviesFragment.kt
+++ app/src/main/java/com/example/moviedatabase/presentation/movies/MoviesFragment.kt
@@ -3,14 +3,14 @@ package com.example.moviedatabase.presentation.movies
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moviedatabase.R
+import com.example.moviedatabase.data.model.movie.Movie
 import com.example.moviedatabase.databinding.FragmentMoviesBinding
 import com.example.moviedatabase.presentation.App
 import com.example.moviedatabase.presentation.di.core.ViewModelModule
@@ -32,11 +32,7 @@ class MoviesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movies, container, false)
 
-        val moviesLiveData = viewModel.getMovies()
-        moviesLiveData.observe(viewLifecycleOwner) {
-            Log.d("MyTag", it.toString())
-        }
-
+        createMenu()
         initRecyclerView()
 
         return binding.root
@@ -49,14 +45,47 @@ class MoviesFragment : Fragment() {
         displayMovieList()
     }
 
-    @SuppressLint("NotifyDataSetChanged")
     private fun displayMovieList() {
-        val moviesLiveData = viewModel.getMovies()
-        moviesLiveData.observe(viewLifecycleOwner) {
-            if (it != null) {
-                adapter.updateList(it)
-                adapter.notifyDataSetChanged()
-            }
+        binding.pbMovies.visibility = View.VISIBLE
+
+        viewModel.getMovies().observe(viewLifecycleOwner) {
+            updateRecyclerView(it)
         }
+    }
+
+    private fun updateMovies() {
+        binding.pbMovies.visibility = View.VISIBLE
+
+        viewModel.updateMovies().observe(viewLifecycleOwner) {
+            updateRecyclerView(it)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun updateRecyclerView(list: List<Movie>?) {
+        if (list != null) {
+            adapter.updateList(list)
+            adapter.notifyDataSetChanged()
+        }
+        binding.pbMovies.visibility = View.GONE
+    }
+
+    private fun createMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object: MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.update_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.bUpdate -> {
+                        updateMovies()
+                        return true
+                    }
+                }
+
+                return false
+            }
+        }, viewLifecycleOwner)
     }
 }
